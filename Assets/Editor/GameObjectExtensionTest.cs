e03e8e2599ac53814086ec0332fd6cc71f537643
anz <anz.factory@gmail.com>
2017-07-14 00:08:47 +0900

    Add GameObjectExtension and test.

---
+++ Assets/Editor/GameObjectExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Xyz.AnzFactory.Utils;
+
+public class GameObjectExtensionTest
+{
+
+	[Test]
+	public void AddComponentIfNeeded_NotYet()
+    {
+        var gameObject = new GameObject("Test");
+        var text = gameObject.AddComponentIfNeeded<Text>();
+
+        Assert.IsTrue(text is Text, "追加されたコンポーネントがちがう！");
+        Assert.IsTrue(gameObject.GetComponents<Text>().Length == 1, "余計にコンポーネントがアタッチされている！");
+	}
+
+    [Test]
+    public void AddComponentIfNeeded_AlreadyExists()
+    {
+        var gameObject = new GameObject("Test");
+        var original = gameObject.AddComponent<Button>();
+        var additional = gameObject.AddComponentIfNeeded<Button>();
+
+        Assert.IsTrue(original == additional, "既にあったのにはりつけてしまっている！");
+        Assert.IsTrue(gameObject.GetComponents<Button>().Length == 1, "余計にコンポーネントがアタッチされている！");
+    }
+
+}
---
+++ Assets/Utils/GameObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xyz.AnzFactory.Utils {
+    public static class GameObjectExtension
+    {
+        public static T AddComponentIfNeeded<T>(this GameObject gameObject) where T:Component
+        {
+            T additionalComponent = gameObject.GetComponent<T>();
+            if (additionalComponent == null) {
+                    additionalComponent = gameObject.AddComponent<T>();
+            }
+            return additionalComponent;
+        }
+    }
+}
