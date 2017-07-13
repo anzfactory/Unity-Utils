35067380b7277b7ae72a47eefe7fcc5398946445
anz <anz.factory@gmail.com>
2017-07-14 00:52:55 +0900

    Add ListExtension and test.

---
+++ Assets/Editor/ListExceptionTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Xyz.AnzFactory.Utils.Extension;
+
+public class ListExceptionTest
+{
+
+	[Test]
+	public void PopTest_Success()
+    {
+        var list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        var last = list.Pop();
+        Assert.IsTrue(last == 3, "最後のアイテムが取れていない！");
+        Assert.IsTrue(list.Count == 2, "リストからアイテムが取り出されていない");
+	}
+
+    [Test]
+    public void PopTest_Failed()
+    {
+        var list = new List<string>();
+
+        System.Exception ex = null;
+        try {
+            list.Pop();
+        } catch (System.Exception e) {
+            ex = e;
+        }
+
+        Assert.IsNotNull(ex, "例外が発生していない！");
+        Assert.IsInstanceOf<System.InvalidOperationException>(ex, "予期せぬ例外！");
+    }
+}
---
+++ Assets/Utils/Extension/ListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xyz.AnzFactory.Utils.Extension
+{
+    public static class ListExtension
+    {
+        public static T Pop<T>(this List<T> self)
+        {
+            if (self.Count == 0) {
+                throw new System.InvalidOperationException("件数が0件");
+            }
+
+            var item = self[self.Count - 1];
+            self.RemoveAt(self.Count - 1);
+            return item;
+        }
+    }
+}
