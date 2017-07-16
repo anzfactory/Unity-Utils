6b6fa95881feddfa1215ce40770effacc06a1d26
anzfactory <anz.factory@gmail.com>
2017-07-17 02:57:49 +0900

    "Add GameObjectExtension.AddEventTrigger()."

--- Assets/Utils/Extension/GameObjectExtension.cs
+++ Assets/Utils/Extension/GameObjectExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Xyz.AnzFactory.Utils.Extension
@@ -11,5 +12,20 @@ namespace Xyz.AnzFactory.Utils.Extension
         {
 			return self.GetComponent<T>() ?? self.AddComponent<T>();
         }
+
+		public static void AddClickEventTrigger(this GameObject self, UnityAction<BaseEventData> callback)
+		{
+			self.AddEventTrigger(EventTriggerType.PointerClick, callback);
+		}
+
+		public static void AddEventTrigger(this GameObject self, EventTriggerType triggerType, UnityAction<BaseEventData> callback)
+		{
+			var entry = new EventTrigger.Entry();
+			entry.eventID = triggerType;
+			entry.callback.AddListener(callback);
+
+			var trigger = self.AddComponentIfNeeded<EventTrigger>();
+			trigger.triggers.Add(entry);
+		}
     }
 }
