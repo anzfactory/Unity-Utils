8156c34a29aec06076ff1019790e84d4e285512b
anzfactory <anz.factory@gmail.com>
2017-07-17 02:25:16 +0900

    Add transform extension.

---
+++ Assets/Utils/Extension/TransformExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xyz.AnzFactory.Utils.Extension
+{
+	public static class TransformExtension
+	{
+		/// <summary>
+		/// 再帰的にLayerを変える
+		/// </summary>
+		public static void ChangeLayersRecursively(this Transform self, string name)
+		{
+			self.gameObject.layer = LayerMask.NameToLayer(name);
+			foreach(Transform child in self) {
+				child.ChangeLayersRecursively(name);
+			}
+		}
+	}
+}
