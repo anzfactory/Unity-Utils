ee42879246304327eb5b55c713d9d6c59dc24f1f
anzfactory <anz.factory@gmail.com>
2017-11-12 12:15:06 +0900

    Add ButtonExtension
    method SetTitle()

---
+++ Assets/Editor/Test/ButtonExtensionTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Xyz.AnzFactory.Utils.Extension;
+
+public class ButtonExtensionTest
+{
+
+	[Test]
+	public void SetTitle()
+	{
+		var title = "ぼたんたいとる";
+		var go = this.CreateButtonObject();
+		var button = go.GetComponent<Button>();
+		button.SetTitle(title);
+
+		Assert.IsTrue(title == go.GetComponentInChildren<Text>().text, "タイトルちがう...");
+	}
+
+
+	private GameObject CreateButtonObject()
+	{
+		// inspectorから作ったような階層を想定している
+		var go = new GameObject();
+		go.AddComponent<Button>();
+		var textGo = new GameObject();
+		textGo.AddComponent<Text>();
+		textGo.transform.SetParent(go.transform, false);
+		return go;
+	}
+}
---
+++ Assets/Utils/Extension/ButtonExtension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+
+/// <summary>
+/// uGUI.Buttonの拡張
+/// </summary>
+public static class ButtonExtension
+{
+	/// <summary>
+	/// Textコンポーネントを探して指定された文字列をセットする
+	/// </summary>
+	/// <param name="self">Self.</param>
+	/// <param name="title">Title.</param>
+	public static void SetTitle(this Button self, string title)
+	{
+		var text = self.GetComponentInChildren<Text>();
+		if (text == null) {
+			UnityEngine.Debug.LogWarning("Not found text compoent");
+			return;
+		}
+		text.text = title;
+	}
+}
