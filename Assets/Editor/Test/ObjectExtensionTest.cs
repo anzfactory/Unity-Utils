3d21824ebb940c1b8abe03f67f2a3b578acd20f1
anzfactory <anz.factory@gmail.com>
2017-11-12 10:48:04 +0900

    Add ObjectExtension
    method ToInt()

---
+++ Assets/Editor/Test/ObjectExtensionTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Xyz.AnzFactory.Utils.Extension;
+
+public class ObjectExtensionTest
+{
+	[Test]
+	public void ToIntTest()
+	{
+		var result = "5".ToInt();
+		Assert.IsTrue(result == 5);
+	}
+
+	[Test]
+	public void ToIntTest_Failed()
+	{
+		var result = "".ToInt();
+		Assert.IsTrue(result == 0);
+		result = "abc".ToInt();
+		Assert.IsTrue(result == 0);
+	}
+
+	[Test]
+	public void ToIntTest_Default()
+	{
+		var result = "abc".ToInt(5);
+		Assert.IsTrue(result == 5);
+		result = "20".ToInt(5);
+		Assert.IsTrue(result == 20);
+	}
+
+}
---
+++ Assets/Utils/Extension/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ObjectExtension
+{
+	public static int ToInt(this object self, int defaultValue = 0)
+	{
+		int result;
+		if (int.TryParse(self.ToString(), out result)) {
+			return result;
+		} else if (result != defaultValue) {
+			return defaultValue;
+		} else {
+			return default(int);
+		}
+	}
+}
